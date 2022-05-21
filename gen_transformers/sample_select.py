6e7d4dcc907ac773a968a318d2e4e3b30401bb38
Griffin Adams <griff4692@gmail.com>
2022-05-21 19:38:36 -0400

    Generate from mask.

--- gen_transformers/sample_select.py
+++ gen_transformers/sample_select.py
@@ -63,6 +63,37 @@ def get_priority(source_toks, summary, nlp):
     return np.argsort(- avg_rs), avg_rs
 
 
+def gen_from_mask(model, tokenizer, source_annotated, idx_to_keep, special_id_min):
+    inputs = tokenizer(
+        [source_annotated],
+        padding='longest',
+        truncation=True,
+        max_length=1024,
+        return_tensors='pt',
+    )
+    input_ids = inputs['input_ids'].to(args.gpu_device)
+    attention_mask = inputs['attention_mask'].to(args.gpu_device)
+    cls_mask = input_ids >= special_id_min
+    updated_mask = sentence_mask(cls_mask, idx_to_keep, attention_mask)
+    kwargs = {
+        'input_ids': input_ids,
+        'attention_mask': updated_mask,
+        'num_return_sequences': 1,
+        'num_beams': 4,
+        'length_penalty': 4.0,
+        'max_length': 142,
+        'min_length': 56,
+        'no_repeat_ngram_size': 3,
+        'early_stopping': True,
+    }
+
+    pred_ids = model.model.generate(**kwargs)
+    pred_str = tokenizer.batch_decode(pred_ids.tolist(), skip_special_tokens=True)[0]
+    rr = compute_rouge([pred_str], [reference], rouge_metric)
+    rr['prediction'] = pred_str
+    return rr
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Sample from Top-K')
 
@@ -72,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('--wandb_name', default='select_extract_abstract')
     parser.add_argument('-debug', default=False, action='store_true')
     parser.add_argument('--hf_model', default='facebook/bart-base')
-    parser.add_argument('--max_examples', default=100, type=int)
+    parser.add_argument('--max_examples', default=25, type=int)
     parser.add_argument('--extract_sent_len', default=3, type=int)
     parser.add_argument('--samples', default=16, type=int)
     parser.add_argument('-use_scores', default=False, action='store_true')
@@ -100,14 +131,14 @@ if __name__ == '__main__':
     ckpt_path = get_path_from_exp(weight_dir, args.wandb_name)
     tokenizer_dir = os.path.join(weight_dir, args.wandb_name, 'tokenizer')
     print(f'Loading tokenizer from {tokenizer_dir}...')
-    # tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=tokenizer_dir)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=tokenizer_dir)
 
-    # additional_ids = tokenizer.additional_special_tokens_ids
-    # special_id_min = 999999 if len(additional_ids) == 0 else min(tokenizer.additional_special_tokens_ids)
+    additional_ids = tokenizer.additional_special_tokens_ids
+    special_id_min = 999999 if len(additional_ids) == 0 else min(tokenizer.additional_special_tokens_ids)
 
     print(f'Loading model from {ckpt_path}...')
-    # model = TransformerSummarizer.load_from_checkpoint(
-    #     checkpoint_path=ckpt_path, tokenizer=tokenizer, hf_model=args.hf_model, strict=False).to(args.gpu_device).eval()
+    model = TransformerSummarizer.load_from_checkpoint(
+        checkpoint_path=ckpt_path, tokenizer=tokenizer, hf_model=args.hf_model, strict=False).to(args.gpu_device).eval()
 
     rouge_metric = RougeMetric()
     configs = [
@@ -145,7 +176,7 @@ if __name__ == '__main__':
             # for idx in implied_idx:
             #     sent_scores[idx] += 0.25
 
-            # source_annotated = ''.join([f'<s{i}> {s}' for i, s in enumerate(source_sents)])
+            source_annotated = ''.join([f'<s{i}> {s}' for i, s in enumerate(source_sents)])
             # Get source tokens
             source_sents_tok = [[str(token.text) for token in sentence] for sentence in source_sents]
             pred_abstract = record['abstract']
@@ -169,6 +200,9 @@ if __name__ == '__main__':
             trunc_idx = min(n, args.k)
             top_k_priority = priority[:trunc_idx]
             p = softmax(scores[:trunc_idx]) if args.use_scores else None
+            topk_idx = top_k_priority[:min(n, args.extract_sent_len)]
+            # To use the bottom is to incorporate trigram blocking
+            # top3 = list(map(int, record['extract_idx'].split(',')))
 
             size = min(n, args.extract_sent_len)
 
@@ -189,11 +223,15 @@ if __name__ == '__main__':
             min_r1 = np.min(r1s)
             max_r1 = np.max(r1s)
 
+            gen_output = gen_from_mask(model, tokenizer, source_annotated, topk_idx, special_id_min)
+
             stats.append({
                 'mean_f1': mean_r1,
                 'min_r1': min_r1,
                 'max_r1': max_r1,
-                'extract_rouge1_f1': record['extract_rouge1_f1']
+                'extract_rouge1_f1': record['extract_rouge1_f1'],
+                'prompt_abstract_rouge1_f1': gen_output['rouge1_f1'],
+                'abstract_rouge1_f1': record['rouge1_f1'],
             })
 
         # out_fn = os.path.join(results_dir, 'extract_samples.csv')
@@ -208,6 +246,6 @@ if __name__ == '__main__':
         print(avgs)
         df.append(avgs)
     df = pd.DataFrame(df)
-    out_fn = os.path.join(results_dir, 'extract_samples.csv')
+    out_fn = os.path.join(results_dir, 'extract_samples_v2.csv')
     df.to_csv(out_fn, index=False)
     print(df.head(len(df)))
