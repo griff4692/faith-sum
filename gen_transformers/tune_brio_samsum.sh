18107dac2cf5540e4e77bfa2650d63279cea645c
Griffin Adams <griff4692@gmail.com>
2022-12-03 14:01:37 -0500

    Update tune hyper-params.

--- gen_transformers/tune_brio_samsum.sh
+++ gen_transformers/tune_brio_samsum.sh
@@ -7,12 +7,14 @@ FT_PATH="/nlp/projects/faithsum/weights/samsum_extract_generator/faith_sum/2olz1
 DEVICE=$1
 LR=3e-5
 MAX_EPOCH=2  # It's quick
+MAX_BRIO_CANDS=8
+PER_DEVICE_BS=4
 
-SHARED="--max_epochs ${MAX_EPOCH} --save_top_k 0 --dataset samsum --lr ${LR} --gpu_device ${DEVICE} --hf_model lidiya/bart-large-xsum-samsum --pretrained_path $FT_PATH -add_brio_loss --extract_method generate --val_metric_mode max --val_monitor_metric extract_mean_f1 --copy_bart_class_dropout 0.1"
+SHARED="--max_epochs ${MAX_EPOCH} --per_device_train_bs ${PER_DEVICE_BS} --save_top_k 0 --max_brio_candidates ${MAX_BRIO_CANDS} --dataset samsum --lr ${LR} --gpu_device ${DEVICE} --hf_model lidiya/bart-large-xsum-samsum --pretrained_path $FT_PATH -add_brio_loss --extract_method generate --val_metric_mode max --val_monitor_metric extract_mean_f1 --copy_bart_class_dropout 0.1"
 
-CWS=(0.1 0.5 1.0 5.0 10.0)
+CWS=(0.1 1.0 10.0)
 MWS=(0.1 0.5 1.0)
-LPS=(0.0 0.5 1.0 2.0 4.0)
+LPS=(0.0 0.5 1.0 4.0)
 SCALES=(0.001 0.01 0.1 1.0)
 
 for CW in "${CWS[@]}"
@@ -26,6 +28,8 @@ do
         EXP_ARGS="--brio_scale ${SCALE} --mle_weight ${MW} --brio_weight ${CW} --brio_length_penalty ${LP}"
         EXP_NAME="samsum_brio_mw_${MW}_cw_${CW}_sc_${SCALE}_lp_${LP}"
         echo "Starting Training for ${EXP_NAME}"
+        echo "${SHARED} ${EXP_ARGS}"
+        exit
         python main.py $SHARED $EXP_ARGS --experiment $EXP_NAME
       done
     done
--- gen_transformers/tune_cnn_samsum.sh
+++ gen_transformers/tune_cnn_samsum.sh
@@ -7,13 +7,14 @@ FT_PATH="/nlp/projects/faithsum/weights/add_doc_bart_large_cnn/faith_sum/3ne4vd5
 
 LR=3e-5
 MAX_EPOCH=1  # It's quick
-MAX_BRIO_CANDS=10
+MAX_BRIO_CANDS=8
+PER_DEVICE_BS=8
 
-SHARED="--max_epochs ${MAX_EPOCH} --max_brio_candidates ${MAX_BRIO_CANDS} --save_top_k 0 --dataset cnn_dailymail --lr ${LR} --gpu_device ${DEVICE} --hf_model facebook/bart-large-cnn --pretrained_path $FT_PATH -add_brio_loss --extract_method generate --val_metric_mode max --val_monitor_metric extract_mean_f1 --copy_bart_class_dropout 0.1"
+SHARED="--max_epochs ${MAX_EPOCH} --per_device_tarin_bs ${PER_DEVICE_BS} --max_brio_candidates ${MAX_BRIO_CANDS} --save_top_k 0 --dataset cnn_dailymail --lr ${LR} --gpu_device ${DEVICE} --hf_model facebook/bart-large-cnn --pretrained_path $FT_PATH -add_brio_loss --extract_method generate --val_metric_mode max --val_monitor_metric extract_mean_f1 --copy_bart_class_dropout 0.1"
 
-CWS=(0.1 0.5 1.0 5.0 10.0)
+CWS=(0.1 1.0 10.0)
 MWS=(0.1 0.5 1.0)
-LPS=(0.0 0.5 1.0 2.0 4.0)
+LPS=(0.0 0.5 1.0 4.0)
 SCALES=(0.001 0.01 0.1 1.0)
 
 for CW in "${CWS[@]}"
