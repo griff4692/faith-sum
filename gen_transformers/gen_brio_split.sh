2fdd78d6f2dc33e005003d40522ebeb245491b6a
Griffin Adams <griff4692@gmail.com>
2023-01-12 17:10:51 -0500

    Update gen_brio_split to take optional chunk param.

--- gen_transformers/gen_brio_split.sh
+++ gen_transformers/gen_brio_split.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -e
 
-DATASET=$1
-EXP=$2
-METHOD=$3  # "beam" "diverse"
-SPLIT=$4
 NCANDS=16
+BSIZE=32
 
-SHARED="--dataset $DATASET --experiment $EXP --summary_style abstract --num_return_sequences $NCANDS --decode_method $METHOD"
-python generate.py $SHARED --split $SPLIT
+DEVICE=$1
+DATASET=$2
+EXP=$3
+METHOD=$4  # "beam" "diverse"
+SPLIT=$5
+
+SHARED="--device $DEVICE --dataset $DATASET --experiment $EXP --summary_style abstract --num_return_sequences $NCANDS --decode_method $METHOD --batch_size ${BSIZE}"
+
+if [ $5 -eq 0 ]; then
+  python generate.py $SHARED --split $SPLIT
+else
+  NUM_CHUNKS=8
+  echo "Running chunk $5 / ${NUM_CHUNKS}"
+  python generate.py $SHARED --split $SPLIT --chunk $5 --num_chunks $NUM_CHUNKS
+fi
