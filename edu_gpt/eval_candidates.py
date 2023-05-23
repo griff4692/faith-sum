c9bb59125adf00c2b7029bd3a98df557d9fe2f73
Griffin Adams <griff4692@gmail.com>
2023-05-23 16:33:37 -0400

    Auto-update.

--- edu_gpt/eval_candidates.py
+++ edu_gpt/eval_candidates.py
@@ -13,7 +13,7 @@ from eval.diversity import diversity_score
 
 
 def get_dataset_idx(x):
-    return x.split('/')[-1].split('_')[0]
+    return x.split('/')[-1].replace('.txt', '').split('_')[0]
 
 
 def compute_stats(prediction, reference, rouge):
@@ -24,7 +24,9 @@ def compute_stats(prediction, reference, rouge):
         'num_toks': num_toks,
         'num_sents': num_sents,
         'rouge1': rouge_obj['rouge1'][0],
-        'rouge2': rouge_obj['rouge2'][0]
+        'rouge2': rouge_obj['rouge2'][0],
+        'rougeL': rouge_obj['rougeL'][0],
+        'rougeLsum': rouge_obj['rougeLsum'][0]
     }
 
 
@@ -35,11 +37,11 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', default='cnn_dailymail')
     parser.add_argument('--extract_experiment', default='cnn_e_final')
     parser.add_argument('--model', default='gpt-3.5-turbo', choices=['text-davinci-003', 'gpt-3.5-turbo'])
-    parser.add_argument('--max_examples', default=16, type=int)
+    parser.add_argument('--mode', default='vanilla')
 
     args = parser.parse_args()
 
-    results_dir = os.path.join('/nlp/projects/faithsum/results', args.extract_experiment, args.model)
+    results_dir = os.path.join('/nlp/projects/faithsum/results', args.extract_experiment, args.mode + '_' + args.model)
 
     fns = list(glob(os.path.join(results_dir, '*.txt')))
 
@@ -66,9 +68,15 @@ if __name__ == '__main__':
         stat_df = pd.DataFrame(stats)
 
         agg_stats.append({
-            'diversity': np.mean(diversity_score(cands)),
+            'diversity': None if len(cands) == 1 else np.mean(diversity_score(cands)),
             'max_rouge1': stat_df.rouge1.max(),
             'avg_rouge1': stat_df.rouge1.mean(),
+            'max_rouge2': stat_df.rouge2.max(),
+            'avg_rouge2': stat_df.rouge2.mean(),
+            'max_rougeL': stat_df.rougeL.max(),
+            'avg_rougeL': stat_df.rougeL.mean(),
+            'max_rougeLsum': stat_df.rougeLsum.max(),
+            'avg_rougeLsum': stat_df.rougeLsum.mean(),
             'avg_sents': stat_df.num_sents.mean(),
             'avg_toks': stat_df.num_toks.mean()
         })
